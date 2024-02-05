871e972454064f564b0c4e695333b4b24bf3889e
Your Name <you@example.com>
2024-02-04 23:36:08 -0800

    sorted the code by functions

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
+// These colors will change the page to light mode.
 let colorOne = `white`;
 let colorBackgroundOne = `orange`;
 let headerOne = `rgb(3, 28, 28)`;
-
+// These colors will reset the page back to the original look.
 let colorTwo = `rgb(3, 28, 28)`;
 let colorBackgroundTwo = `rgb(246, 144, 20)`;
 let headerTwo = "rgb(7, 6, 18)";
 
+// This function the the button that changes the page from dark to light theme
 const footerBtn = document.getElementById(`footerBtn`);
 const navBtn = document.getElementById(`navBtn`);
 document.getElementById("mainClick").addEventListener("click", function () {
-  mainClick.style.backgroundColor = colorOne;
-  secondBtn.style.backgroundColor = colorOne;
   document.body.style.background = colorBackgroundOne;
   mainClick.style.color = headerOne;
   secondBtn.style.color = headerOne;
   footerBtn.style.color = headerOne;
   navBtn.style.color = headerOne;
+  document.getElementById("mainClick").style.Color = colorOne;
   document.querySelector(`p`).style.color = headerOne;
   document.querySelector(`h1`).style.color = headerOne;
   document.getElementById(`pBackground`).style.backgroundColor = colorOne;
-  document.getElementById("mainClick").style.Color = `black`;
+  mainClick.style.backgroundColor = colorOne;
+  secondBtn.style.backgroundColor = colorOne;
   footerBtn.style.background = colorOne;
   navBtn.style.background = colorOne;
 });
+// This function the the button that changes the page from light to dark theme
 document.getElementById("secondBtn").addEventListener("click", function () {
   document.body.style.backgroundColor = headerTwo;
   document.querySelector(`p`).style.color = colorBackgroundTwo;
